815fe0d77a39a5490fbdd1a98c4a9dc044084a66
jfalkson <jfalkson@gmail.com>
2013-11-25 20:12:14 -0500

    exercise 1 donezo

--- erb_example.rb
+++ erb_example.rb
@@ -3,3 +3,12 @@ require 'erb'
 x = 42
 template = ERB.new "The value of x is: <%= x %>"
 puts template.result(binding)
+
+puts "Hi, my name is Joe!"
+
+array=[1,2,4]
+
+array.each do |x|
+	puts x**2
+end
+	
